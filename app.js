a03e75dca355259e005dafe09eb3d9c41a28da9c
Janne Mononen <janne.m92@gmail.com>
2015-09-08 15:04:36 +0300

    Lisäsin serverin ja serveriin tarvittavat moduulit, lisäsin myös js-tiedoston itse peliä varten. Pelin lähdekooditiedoston ja css-tiedoston siirsin public kansioon ja laitoin serverin löytämään nämä tiedostot. Siinäpä pitäisi olla pohja valmiina mihin voi sitten ruveta toiminnalisuuksia tekemään :)

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var fs = require("fs");
+
+
+app.use("/public",express.static("public"));
+
+
+app.get('/', function (req,res){
+	res.sendfile("matopeli.html");
+});
+
+io.on("connection", function (socket){
+
+	socket.on("ping", function(){
+		socket.emit("pong");
+	});
+
+	console.log("User connected!");
+});
+
+http.listen(3000, function(){
+	console.log("Server running at port 3000");
+});
---
+++ worm.js
@@ -0,0 +1,1 @@
+document.getElementById("testi").innerHTML = "Taitaa toimia";
