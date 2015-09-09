abd43015115834647458a2a7bbc152f1911adc8e
Janne Mononen <janne.m92@gmail.com>
2015-09-09 17:59:50 +0300

    yritin lisätä matoon liikettä, ei liiku vielä ja kahtelen huomenna koko homman uusiks

--- public/worm.js
+++ public/worm.js
@@ -1,45 +1,58 @@
 var snake = []
 var direction = 1; //oikealle
-var START_LENGTH = 8;
+var START_LENGTH = 4;
+var fps = 5;
 
 
 function initGameBoard(height,width){
+
+	//Tällä loopilla tehdään madosta startlengthin mittainen
 	for ( i = 0; i < START_LENGTH; i++){
-   		var row = Math.floor((width/2));
-    	var data = Math.floor((height/2) - i);
-   	    var locCoord = [row,data]
-        snake.push(locCoord);
+   		var row = Math.floor((width/2)); //Asetetaan riviksi keskimmäinen rivi
+    	var data = Math.floor((height/2) - i); //asetetaan dataksi keskimmäinen td - kierroksen numero
+   	    var locCoord = [row,data] //Tehdään taulukko joka sisältää kaksi arvoa, rivin ja sarakkeen
+        snake.push(locCoord); //lisätään äsken luotu taulu käärmeen kohdaksi
     }    
 
 
-console.log("Gameboard init");
-var gameboard = '<table id="gameboard">'
+	var gameboard = '<table id="gameboard">'
 
-for(i = 0; i<height; i++){
-	gameboard += '<tr>';
-	for (j = 0; j < width; j++){
-		var id = "r"+i+"d"+j;
-		var grid = '<td id="'+id+'">';
-		gameboard += grid;
+	for(i = 0; i<height; i++){
+		gameboard += '<tr>';
+		for (j = 0; j < width; j++){
+			var id = "r"+i+"d"+j;
+			var grid = '<td id="'+id+'">';
+			gameboard += grid;
+		}
+		gameboard += '</tr>';
 	}
-	gameboard += '</tr>';
+	gameboard += '</table>';
+	$("#gameboard_div").html(gameboard);
+	drawSnake(snake);
 }
-gameboard += '</table>';
-$("#gameboard_div").html(gameboard);
-drawSnake(snake);
-}
-
+initGameBoard(33,33);
 
 function drawSnake(snakeLoc){
-	console.log(snakeLoc);
-	var length = snake.length;
-	for(i=0; i< length;i++){
-		var row = snakeLoc[i][0];
-		var data = snakeLoc[i][1];
-		$("#r"+row+"d"+data).css("background", "red");
-	}
-	
+	snakeCoordinates = snakeLoc;
+	setTimeout(function(){
+		console.log("ruudun päivitys");
+		requestAnimationFrame(drawSnake);
+		//console.log("Snake head at row: " +snakeCoordinates[0][0].toString()+" data:" + snakeCoordinates[0][1].toString() );
+		var length = snakeCoordinates.length;
+		for(i=0; i< length;i++){
+			var row = snakeCoordinates[i][0];
+			var data = snakeCoordinates[i][1];
+			$("#r"+row+"d"+data).css("background", "green");	
+		}
+
+		if(direction == 1){//Mennään oikealle eli lisätään datan arvoa
+			for(i=0; i< length;i++){
+				console.log("Lisätään datan arvoa yhdellä");
+			 	snakeCoordinates[i][1] += 1;
+		}
+		}
+	},1000/fps);	
 }
 
 
-initGameBoard(33,33);
+
